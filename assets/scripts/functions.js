0d173d69c923d42aacb9218e0e73b0b3bcdb5a8e
AbnerSaavedra <ingenieroabnersaavedra@gmail.com>
2020-08-12 15:49:31 -0400

    Mejoras de maquetación, flujo e interactividad

---
+++ assets/scripts/functions.js
@@ -0,0 +1,47 @@
+function pintarEficacia(){
+    
+    let color = "#004587";
+    let elements;
+
+    console.log("Aquí");
+    elements = document.getElementsByClassName("eficacia");
+    elements[0].style.backgroundColor = color;
+    var URLactual = window.location.pathname;
+    alert(URLactual);
+}
+
+function pintarAdmon(){
+
+    let color = "#004587";
+    let elements;
+
+    console.log("Aquí");
+    elements = document.getElementsByClassName("admon");
+    elements[0].style.backgroundColor = color;
+    var URLactual = window.location.pathname;
+    alert(URLactual);
+}
+
+function pintarCovid(){
+
+    let color = "#004587";
+    let elements;
+
+    console.log("Aquí");
+    elements = document.getElementsByClassName("covid");
+    elements[0].style.backgroundColor = color;
+    var URLactual = window.location.pathname;
+    alert(URLactual);
+}
+
+function pintarUso(){
+
+    let color = "#004587";
+    let elements;
+
+    console.log("Aquí");
+    elements = document.getElementsByClassName("uso");
+    elements[0].style.backgroundColor = color;
+    var URLactual = window.location.pathname;
+    alert(URLactual);
+}
